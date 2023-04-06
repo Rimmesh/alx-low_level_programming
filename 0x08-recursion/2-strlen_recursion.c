af1b3fa00a19235aa9598b4e7cf16eb69e80d0b1
Rimmesh <rimmeshete@gmail.com>
2023-04-06 12:58:02 -0700

    commit

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,10 +7,8 @@
  */
 int _strlen_recursion(char *s)
 {
-	int a = 0;
-
 	if (*s == '\0')
 		return (0);
-	else 
-		return ( 1 + _strlen_recursion( s + 1 );
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
