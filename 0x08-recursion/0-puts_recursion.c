8d315736dbf3a72217c8548e9aa65d35021ee516
Rimmesh <rimmeshete@gmail.com>
2023-04-06 10:29:10 -0700

    commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,11 @@
 
 void _puts_recursion(char *s);
 {
-if(*s)
-{
-_putchar(*s);
-_put_recursion(s+1);
-}
-else 
-_putchar('\n');
+	if (*s)
+	{
+		_putchar(*s);
+		_put_recursion(s + 1);
+	}
+	else 
+		_putchar('\n');
 }
