b74822818bdd45d73fc6768be9f09f668bfee6b3
Rimmesh <rimmeshete@gmail.com>
2023-04-06 12:16:53 -0700

    commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN.H
+#ifndef MAIN_H
 #define MAIN_H
 
 void _puts_recursion(char *s);
@@ -10,5 +10,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
+int _putchar(char c);
 #endif
+
+
