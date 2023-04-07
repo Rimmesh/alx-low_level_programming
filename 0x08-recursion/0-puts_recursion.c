8ded3d20d9abc69f1d5ccfaa3bf735f272113ed9
Rimmesh <rimmeshete@gmail.com>
2023-04-06 19:20:25 -0700

    commmit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,17 @@
 #include "main.h"
-
 /**
- *  _puts_recursion - function like puts();
- *  Return: Always 0
- *  @s: input.
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
+
 	else
 		_putchar('\n');
 }
-
-
-
