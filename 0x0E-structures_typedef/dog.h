918285173638c14fa6dc37eb09a258a9f2e402c7
Rimmesh <rimmeshete@gmail.com>
2023-04-17 15:17:06 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,19 +6,26 @@
  * @age: 2nd info about the dog( it s age).
  * @owner: 3rd info about the dog(it s owner).
  *
- * Description: a struct "dog" that contains name age and owner of the dog.
+ * Description: struct "dog" that contains name age and owner of the dog.
  */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+/**
+ * dog_t - typedef for the struct dog
+ */
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+
 #endif
+
