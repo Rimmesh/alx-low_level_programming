b234e572aaae6af6429fd71cea99c324c8a8afe3
Rimmesh <rimmeshete@gmail.com>
2023-04-18 15:11:49 -0700

    commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - writes the name of the file it compiles from
+ *
+ * Return: 0 in success always.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
