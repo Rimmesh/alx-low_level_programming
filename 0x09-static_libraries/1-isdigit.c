5248071e7ac871cdbb27075484a7097e4eba9fb4
Rimmesh <rimmeshete@gmail.com>
2023-10-02 16:35:17 -0700

    commit

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-		
 /**
  * _isdigit - checks for a digit
  * @c: int to be checked
@@ -7,6 +6,6 @@
  */
 int _isdigit(int c)
 {
-	return (c >= '0' && c <= '9');
+return (c >= '0' && c <= '9');
 }
 
