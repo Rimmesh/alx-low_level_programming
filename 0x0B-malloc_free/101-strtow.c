38ba04a1458d183d5edb835dcd6b53317d0b006c
Rimmesh <rimmeshete@gmail.com>
2023-10-04 15:50:43 -0700

    commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -46,7 +46,7 @@ if (str[i] == ' ' || str[i] == '\0')
 if (c)
 {
 end = i;
-tmp = (char *)malloc(sizeof(char)*(c + 1));
+tmp = (char *)malloc(sizeof(char) * (c + 1));
 if (tmp == NULL)
 return (NULL);
 while (start < end)
