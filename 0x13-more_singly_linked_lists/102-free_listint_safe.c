9da9d3d8183d4ef8701febcc648135fe24710eea
Rimmesh <rimmeshete@gmail.com>
2023-10-24 14:41:49 -0700

    commit

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -5,7 +5,7 @@
  * _ra - reallocates memory for an array of pointers
  * to the nodes in a linked list
  * @list: old list to append
- * @size: size of the new list 
+ * @size: size of the new list
  * @new: new node to add
  * Return: pointer to the new list
  */
